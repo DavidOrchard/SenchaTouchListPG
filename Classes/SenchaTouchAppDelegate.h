8b232e4b8e3254edd8fc17f98a28e70eee56822d
David Orchard <orchard@pacificspirit.com>
2010-11-03 09:02:33 -0700

    first commit

---
+++ Classes/SenchaTouchAppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SenchaTouchAppDelegate.h
+//  SenchaTouch
+//
+//  Created by Jesse MacFadyen on 10-10-29.
+//  Copyright Nitobi 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "PhoneGapDelegate.h"
+
+@interface SenchaTouchAppDelegate : PhoneGapDelegate {
+}
+
+@end
+
---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SenchaTouch
+//
+//  Created by Jesse MacFadyen on 10-10-29.
+//  Copyright Nitobi 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"SenchaTouchAppDelegate");
+    [pool release];
+    return retVal;
+}
